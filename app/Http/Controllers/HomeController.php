193d99d2f7361c67575369bdcabc2d528ad41d0f
Developer <alexxx-ivanow>
2020-11-19 18:41:10 +0300

    Изучение настроек в Laravel - урок 9

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,8 @@ namespace App\Http\Controllers;
 class HomeController extends Controller {
 
     public function index() {
-
+        dump($_ENV);
+        dump(config('auth.guards.api.driver'));
         return view('home', ['res' => 5, 'name' => 'Alexey']);
 
     }
