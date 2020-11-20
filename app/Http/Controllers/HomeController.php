0df691d1991a03400c851675093cd7de968394d4
Developer <alexxx-ivanow>
2020-11-20 14:58:01 +0300

    Выполнение sql-запросов и транзакций в laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,12 +9,30 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller {
 
     public function index() {
-        dump($_ENV);
-        dump(config('auth.guards.api.driver'));
-        return view('home', ['res' => 5, 'name' => 'Alexey']);
+        /*$query = DB::insert("INSERT INTO posts (title, text) VALUES (?, ?)", ['Статья 5', 'Text 5']);
+        var_dump($query);*/
+
+        /*DB::update("UPDATE posts SET created_at = ?, updated_at = ? WHERE created_at IS NULL OR updated_at IS NULL", [now(), now()]);*/
+
+        DB::beginTransaction();
+        try {
+            DB::update("UPDATE posts SET created_at = ? WHERE created_at IS NULL", [now()]);
+            DB::update("UPDATE posts SET updated_at = ? WHERE updated_at IS NULL", [now()]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+
+        DB::delete("DELETE FROM posts WHERE id = ?", [4]);
+
+        $posts = DB::select("SELECT *FROM posts WHERE id > :id", ['id' => 2]);
+        return $posts;
 
     }
 
